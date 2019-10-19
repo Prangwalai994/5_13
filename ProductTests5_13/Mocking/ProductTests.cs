c86c3ddf9cf874d8405a3b44d54ea40b6d611c4e
Prangwalai994 <prangwalai.li@rmuti.ac.th>
2019-10-20 05:05:03 +0700

    5_13 Modify 2

--- ProductTests5_13/Mocking/ProductTests.cs
+++ ProductTests5_13/Mocking/ProductTests.cs
@@ -1,4 +1,5 @@
 ﻿using An_Example_of_a_Mock_Abuse5_13.Mocking;
+using Moq;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
@@ -17,6 +18,17 @@ namespace ProductTests5_13.Mocking
             var product = new Product { ListPrice = 100 };
 
             var result = product.GetPrice(new Customer { IsGold = true });
+
+            Assert.That(result, Is.EqualTo(70));
+        }
+        [Test]
+        public void GetPrice_GlodCustomer_Apply30PercentDiscout2()
+        {
+            var customer = new Mock<ICustomer>();
+            customer.Setup(c => c.IsGold).Returns(true);
+            var product = new Product { ListPrice = 100 };
+
+            var result = product.GetPrice(customer.Object);
 
             Assert.That(result, Is.EqualTo(70));
         }
